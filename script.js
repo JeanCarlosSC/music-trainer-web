56a32e3f4b7cb56cfd0a07ffaa643c9872248f75
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-03-10 17:00:06 -0500

    simplified index.html and documenting script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-let lastKey = "";
+// script.js
+const keys = ["Ab", "A",  "Bb", "B", "Cb", "C", "C#", "Db", "D", "Eb", "E", "F", "F#", "Gb", "G"] // keys
+let lastKey = ""; // so as not to repeat key
 
+// create 240 paragraphs in the table div
+for(let i=0; i<240; i++) {
+    let paragraph = document.createElement("p");
+    document.getElementById("table").appendChild(paragraph);
+}
+
+// updates each paragraph to a random key
 let field = document.querySelectorAll('p');
-console.log(field);
 field.forEach(element => element.textContent = getRandomKey());
 
+// return a random string key from keys without consecutive repetition
 function getRandomKey() {
-    const keys = ["Ab", "A",  "Bb", "B", "Cb", "C", "C#", "Db", "D", "Eb", "E", "F", "F#", "Gb", "G"]
-    const item = keys[Math.floor(Math.random()*keys.length)];
-    if(lastKey === item) {
-        return getRandomKey();
+    const randomKey = keys[Math.floor(Math.random()*keys.length)]; // random key
+
+    // doesn't repeat consecutive key
+    if(lastKey === randomKey) {
+        return getRandomKey(); // pick another random key
     }
-    lastKey = item;
-    return item;
+    lastKey = randomKey; // save key to avoid repetition
+
+    return randomKey;
 }
